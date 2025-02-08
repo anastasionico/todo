a29926b1eca3cda8fea572c4314ab3591ba73138
Nico Anastasio <nico@anastasionico.uk>
2025-02-08 10:30:38 +0000

    refactoring to functions

--- todo.py
+++ todo.py
@@ -1,23 +1,29 @@
-"""
-    ask the user if he want to 
-Read
-    #show the todo list
-
-Delete
-    #get the position from the user
-    #delete the set element with that position
-    #show the todo list
-
-"""
 task = []
-i = 1
 
-while i < 10: 
-    operation = input('type what operation you want to perform [1] create a new task [2] show all the existing tasks [3] delete a task\n')
+def main():
+    i = 1  
+    while i < 10: 
+        operation = input('type what operation you want to perform [1] create a new task [2] show all the existing tasks [3] delete a task\n')
 
-    operation = int(operation)
+        operation = int(operation)
+
+        process(operation)
+        
+        i += 1
+    else:
+        print("program concluded\n")
 
 
+def delete_task():
+    element = input("insert the number of the element you want to delete\n")
+    
+    if int(element) > len(task):
+        print("the number needs to be under {len(task)}\n")
+    else:
+        task.pop(int(element))
+
+
+def process(operation):
     if operation == 1:
         task.append(input("Insert the name of the task you want to add\n"))
 
@@ -25,14 +31,8 @@ while i < 10:
         print(task)
 
     elif operation == 3:
-        element = input("insert the number of the element you want to delete\n")
-    
-        if int(element) > len(task):
-            print("the number needs to be under {len(task)}\n")
-        else:
-            task.pop(int(element))
+       delete_task() 
     else:
         exit('program concluded\n')
-    i += 1
-else:
-    print("program concluded\n")
+
+main()
